789259be94b654d645fb3f55153d1bdc8e1254e0
isstrick <isstrick@ncsu.edu>
2025-02-23 15:05:08 -0500

    Initial set-up

---
+++ level.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Cursor.global_position = get_global_mouse_position()
---
+++ scripts/room_slot.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+var occupied = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Globals.is_dragging:
+		$Sprite2D.modulate = Color.GREEN_YELLOW
+	else:
+		$Sprite2D.modulate = Color.DIM_GRAY
